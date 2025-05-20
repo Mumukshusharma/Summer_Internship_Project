0e829ea73bb8929030deae07891919007613d832
Mumukshusharma <mumukshsharma123@gmail.com>
2025-05-20 22:50:23 +0530

    adding movies

--- content-data.js
+++ content-data.js
@@ -9,9 +9,10 @@ const animeShows = [
   const animeMovies = [
     { title: 'Spirited Away', imgSrc: 'https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,0,380,562_.jpg', description: 'A magical journey into a mysterious world' },
     { title: 'Your Name', imgSrc: 'https://picfiles.alphacoders.com/504/50493.jpg', description: 'A beautiful tale of two strangers whose lives are mysteriously linked' },
-    { title: 'Weathering with You', imgSrc: 'https://m.media-amazon.com/images/M/MV5BMDkwMWFiMTgtMTgxZC00ZTU1LWI0N2ItMGFiZTdjMDQ1YTE4XkEyXkFqcGc@._V1_.jpg', description: 'Weathering with You is a lyrical journey where love’s light intertwines with the storm, reshaping the world’s skies' },
+    { title: 'Weathering with You', imgSrc: 'https://static.wikia.nocookie.net/tenkinoko1663/images/c/c9/Key_Visual_3.png/revision/latest?cb=20201124164242', description: 'Weathering with You is a lyrical journey where love’s light intertwines with the storm, reshaping the world’s skies' },
     { title: '5 Centimeters per Second', imgSrc: 'https://m.media-amazon.com/images/M/MV5BODVmZjhhYTYtYzRiOC00YzFiLThlZjMtZTQxNWY0MTI1MzlmXkEyXkFqcGc@._V1_FMjpg_UX739_.jpg', description: 'A poignant tale of love and separation, where time drifts like falling petals.' },
     { title: 'Grave of the Fireflies', imgSrc: 'https://m.media-amazon.com/images/M/MV5BZmY2NjUzNDQtNTgxNC00M2Q4LTljOWQtMjNjNDBjNWUxNmJlXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg', description: 'A tender, haunting tale of innocence lost amidst the darkness of war' },
+    { title: 'Howls Moving Castle', imgSrc: 'https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/howls_moving_castle_1_9.png', description: 'A tender, haunting tale of innocence lost amidst the darkness of war' },
   ];
   
   const animeShowsGrid = document.getElementById('anime-shows');
